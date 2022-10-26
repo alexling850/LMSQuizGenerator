f8182a9fcf9105ad87cd33f36bf1d63df97cb01c
Alex Ling <lingc@algonquincollege.com>
2022-10-26 17:00:07 -0400

    first commit.

---
+++ QuizParser/QuestionType.cs
@@ -0,0 +1,17 @@
+﻿namespace QuizParseLibrary
+{
+    public enum QuestionType
+    {
+        QT_LEGACY_HEADER = -2,
+        QT_UNKNOWN = -1,
+        QT_MULTIPLE_CHOICE,
+        QT_MULTIPLE_ANSWER,
+        QT_TRUE_OR_FALSE,
+        QT_ESSAY,
+        QT_FILL_IN_THE_BLANK,
+        QT_MATCHING,
+        QT_ORDERING,
+        QT_TEXT,
+        QT_IMAGE
+    }
+}
